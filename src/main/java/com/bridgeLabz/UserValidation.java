fd7262fd5eceec9a78d5b3b047a03ef84d587725
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-25 12:15:38 +0530

    [Added] User First Name

--- src/main/java/com/bridgeLabz/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserValidation.java
@@ -1,7 +1,28 @@
 package com.bridgeLabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidation {
+    static Scanner scanner = new Scanner(System.in);
+    private static void User_FirstName() {
+        //UC1 User need to enter valid First Name
+        String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        System.out.println("Enter First Name:");
+        String First_Name = scanner.next();
+
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(First_Name);
+
+        boolean result = matcher.matches();
+        if(result == true)
+            System.out.println("Valid");
+        else
+            System.out.println("Not valid");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
+        User_FirstName();
     }
 }
