b4934f932613dc497d26579e36a16c8f89804314
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-25 13:38:13 +0530

    [Added] Validate Multiple Emails

--- src/main/java/com/bridgeLabz/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserValidation.java
@@ -128,7 +128,144 @@ public class UserValidation {
         else
             System.out.println("Not valid");
     }
-    public static void main(String[] args) {
+    private static void MultipleEmails() {
+        //UC9 Should clear all email samples
+        String email1 = "abc@yahoo.com";
+        String email2 = "abc-100@yahoo.com";
+        String email3 = "abc.100@yahoo.com";
+        String email4 = "abc111@abc.com";
+        String email5 = "abc-100@abc.net";
+        String email6 = "abc.100@abc.com.au";
+        String email7 = "abc@1.com";
+        String email8 = "abc@gmail.com";
+        String email9 = "abc+100@gmail.com";
+        String email10 = "abc";
+        String email11 = "abc@.com.my";
+        String email12 = "abc123@gmail.a";
+        String email13 = "abc123@.com";
+        String email14 = "abc123@.com.com";
+        String email15 = ".abc@abc.com";
+        String email16 = "abc()*@gmail.com";
+        String email17 = "abc@%*.com";
+        String email18 = "abc..2002@gmail.com";
+        String email19 = "abc.@gmail.com";
+        String email20 = "abc@abc@gmail.com";
+        String email21 = "abc@gmail.com.1a";
+        String email22 = "abc@gmail.com.aa.au";
+
+        String MULTIPLE_EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9]+)*@" + "(?:[a-zA-Z0-9]+\\.)+[a-zA-Z]{2,7}$";
+
+        Pattern patterns1 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers1 = patterns1.matcher(email1);
+        boolean results1 = matchers1.matches();
+        System.out.println(results1);
+
+        Pattern patterns2 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers2 = patterns2.matcher(email2);
+        boolean results2 = matchers2.matches();
+        System.out.println(results2);
+
+        Pattern patterns3 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers3 = patterns3.matcher(email3);
+        boolean results3 = matchers3.matches();
+        System.out.println(results3);
+
+        Pattern patterns4 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers4 = patterns4.matcher(email4);
+        boolean results4 = matchers4.matches();
+        System.out.println(results4);
+
+        Pattern patterns5 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers5 = patterns5.matcher(email5);
+        boolean results5 = matchers5.matches();
+        System.out.println(results5);
+
+        Pattern patterns6 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers6 = patterns6.matcher(email6);
+        boolean results6 = matchers6.matches();
+        System.out.println(results6);
+
+        Pattern patterns7 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers7 = patterns7.matcher(email7);
+        boolean results7 = matchers7.matches();
+        System.out.println(results7);
+
+        Pattern patterns8 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers8 = patterns8.matcher(email8);
+        boolean results8 = matchers8.matches();
+        System.out.println(results8);
+
+        Pattern patterns9 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers9 = patterns9.matcher(email9);
+        boolean results9 = matchers9.matches();
+        System.out.println(results9);
+
+        Pattern patterns10 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers10 = patterns10.matcher(email10);
+        boolean results10 = matchers10.matches();
+        System.out.println(results10);
+
+        Pattern patterns11 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers11 = patterns11.matcher(email11);
+        boolean results11 = matchers11.matches();
+        System.out.println(results11);
+
+        Pattern patterns12 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers12 = patterns12.matcher(email12);
+        boolean results12 = matchers12.matches();
+        System.out.println(results12);
+
+        Pattern patterns13 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers13 = patterns13.matcher(email13);
+        boolean results13 = matchers13.matches();
+        System.out.println(results13);
+
+        Pattern patterns14 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers14 = patterns14.matcher(email14);
+        boolean results14 = matchers14.matches();
+        System.out.println(results14);
+
+        Pattern patterns15 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers15 = patterns15.matcher(email15);
+        boolean results15 = matchers15.matches();
+        System.out.println(results15);
+
+        Pattern patterns16 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers16 = patterns16.matcher(email16);
+        boolean results16 = matchers16.matches();
+        System.out.println(results16);
+
+        Pattern patterns17 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers17 = patterns17.matcher(email17);
+        boolean results17 = matchers17.matches();
+        System.out.println(results17);
+
+        Pattern patterns18 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers18 = patterns18.matcher(email18);
+        boolean results18 = matchers18.matches();
+        System.out.println(results18);
+
+        Pattern patterns19 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers19 = patterns19.matcher(email19);
+        boolean results19 = matchers19.matches();
+        System.out.println(results19);
+
+        Pattern patterns20 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers20 = patterns20.matcher(email20);
+        boolean results20 = matchers20.matches();
+        System.out.println(results20);
+
+        Pattern patterns21 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers21 = patterns21.matcher(email21);
+        boolean results21 = matchers21.matches();
+        System.out.println(results21);
+
+        Pattern patterns22 = Pattern.compile(MULTIPLE_EMAIL_PATTERN);
+        Matcher matchers22 = patterns22.matcher(email22);
+        boolean results22 = matchers22.matches();
+        System.out.println(results22);
+    }
+        public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
         User_FirstName();
         User_LastName();
@@ -138,5 +275,6 @@ public class UserValidation {
         User_Password_Rule1();
         User_Password_Rule2();
         User_Password_Rule3();
+        MultipleEmails();
     }
 }
